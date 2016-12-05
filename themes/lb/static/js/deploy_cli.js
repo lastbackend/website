ad70749bfadda509704d7c6c452cfca61b97e45d
lavr11 <lavrentev.srg@gmail.com>
2016-12-05 22:56:13 +0300

    Init commit

---
+++ themes/lb/static/js/deploy_cli.js
@@ -0,0 +1,60 @@
+'use strict';
+
+window.onload = function show_deploy_cli() {
+    var i = 0;
+    var text = '';
+
+    // while (i < 53) {
+    //     if (i == 0) {
+    //         text += "# try 'deploy help' for all commands" + '<br>';
+    //         i++;
+    //         setTimeout(printText(text), 1000);
+    //     }
+    //
+    //     if (i == 1) {
+    //         text += '# ~ your-project $ deploy it to production' + '<br>';
+    //         i++;
+    //         setTimeout(printText(text), 1000);
+    //     }
+    //
+    //     if (i == 2) {
+    //         text += '> Build [=';
+    //         i++;
+    //         setTimeout(printText(text), 1000);
+    //         while (i < 28) {
+    //             i++;
+    //             text += '=';
+    //             setTimeout(printText(text), 1000);
+    //         }
+    //         text += '] 100% 6.9s' + '<br>';
+    //         setTimeout(printText(text), 1000);
+    //         text += '> Building complete!' + '<br>';
+    //         setTimeout(printText(text), 1000);
+    //     }
+    //
+    //     if (i == 28) {
+    //         text += '> Deploy [=';
+    //         i++;
+    //         setTimeout(printText(text), 1000);
+    //         while (i < 53) {
+    //             i++;
+    //             text += '=';
+    //             setTimeout(printText(text), 1000);
+    //         }
+    //         text += '] 100% 2.2s' + '<br>';
+    //         setTimeout(printText(text), 1000);
+    //         text += '> Deploy success!' + '<br>';
+    //         setTimeout(printText(text), 1000);
+    //     }
+    // }
+
+    // text += '> Complete! https://your-project.yourdomain.com';
+    // setTimeout(printText(text), 1000);
+    setTimeout(printText('lal'), 1000);
+    setTimeout(printText('lol'), 1000)
+};
+
+function printText(text) {
+    var p = document.querySelector('.new_cli_text');
+    p.innerHTML = text;
+}
---
+++ themes/lb/static/js/logs/lbInit.js
@@ -0,0 +1,12 @@
+/**
+ * @namespace print
+ */
+print('<span class="w-cli__om">Connect to server</span>');
+print('<span class="w-cli__om">Create app </span><span class="w-cli__oi">hello-world</span>.', 500);
+print('<span class="w-cli__om">Create app </span><span class="w-cli__oi">hello-world</span>..', 500);
+print('<span class="w-cli__om">Create app </span><span class="w-cli__oi">hello-world</span>...', 500);
+print(' ', 500);
+print('<span class="w-cli__om">Application </span><span class="w-cli__os">hello-world</span> has been created');
+print(' ', 0);
+print('<span class="w-cli__om">Push you sources to deploy app', 0);
+print(' ', 0);
