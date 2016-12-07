5418b21dfce92b9cc1f4566eb6e887aaa291cf3f
lavr11 <lavrentev.srg@gmail.com>
2016-12-07 12:51:52 +0300

    Some fix

--- themes/lb/static/js/deploy_cli.js
+++ themes/lb/static/js/deploy_cli.js
@@ -1,60 +1,60 @@
-// 'use strict';
-//
-// window.onload = function show_deploy_cli() {
-//     var i = 0;
-//     var text = '';
-//
-//     // while (i < 53) {
-//     //     if (i == 0) {
-//     //         text += "# try 'deploy help' for all commands" + '<br>';
-//     //         i++;
-//     //         setTimeout(printText(text), 1000);
-//     //     }
-//     //
-//     //     if (i == 1) {
-//     //         text += '# ~ your-project $ deploy it to production' + '<br>';
-//     //         i++;
-//     //         setTimeout(printText(text), 1000);
-//     //     }
-//     //
-//     //     if (i == 2) {
-//     //         text += '> Build [=';
-//     //         i++;
-//     //         setTimeout(printText(text), 1000);
-//     //         while (i < 28) {
-//     //             i++;
-//     //             text += '=';
-//     //             setTimeout(printText(text), 1000);
-//     //         }
-//     //         text += '] 100% 6.9s' + '<br>';
-//     //         setTimeout(printText(text), 1000);
-//     //         text += '> Building complete!' + '<br>';
-//     //         setTimeout(printText(text), 1000);
-//     //     }
-//     //
-//     //     if (i == 28) {
-//     //         text += '> Deploy [=';
-//     //         i++;
-//     //         setTimeout(printText(text), 1000);
-//     //         while (i < 53) {
-//     //             i++;
-//     //             text += '=';
-//     //             setTimeout(printText(text), 1000);
-//     //         }
-//     //         text += '] 100% 2.2s' + '<br>';
-//     //         setTimeout(printText(text), 1000);
-//     //         text += '> Deploy success!' + '<br>';
-//     //         setTimeout(printText(text), 1000);
-//     //     }
-//     // }
-//
-//     // text += '> Complete! https://your-project.yourdomain.com';
-//     // setTimeout(printText(text), 1000);
-//     setTimeout(printText('lal'), 1000);
-//     setTimeout(printText('lol'), 1000)
-// };
-//
-// function printText(text) {
-//     var p = document.querySelector('.new_cli_text');
-//     p.innerHTML = text;
-// }
+'use strict';
+
+function whilefun() {
+    var i = 0;
+    var text = '';
+
+    while (i < 53) {
+        if (i == 0) {
+            text += "# try 'deploy help' for all commands" + '<br>';
+            setTimeout(function(){printText(text)}, 1000);
+        }
+
+        if (i == 1) {
+            text += '# ~ your-project $ deploy it to production' + '<br>';
+            setTimeout(function(){printText(text)}, 1000);
+        }
+
+        if (i == 2) {
+            text += '> Build [=';
+            setTimeout(function(){printText(text)}, 1000);
+            while (i < 28) {
+                i++;
+                text += '=';
+                setTimeout(printText(text), 1000);
+            }
+            text += '] 100% 6.9s' + '<br>';
+            setTimeout(function(){printText(text)}, 1000);
+            text += '> Building complete!' + '<br>';
+            setTimeout(function(){printText(text)}, 1000);
+        }
+
+        if (i == 28) {
+            text += '> Deploy [=';
+            setTimeout(function(){printText(text)}, 1000);
+            while (i < 53) {
+                i++;
+                text += '=';
+                setTimeout(printText(text), 1000);
+            }
+            text += '] 100% 2.2s' + '<br>';
+            setTimeout(function(){printText(text)}, 1000);
+            text += '> Deploy success!' + '<br>';
+            setTimeout(function(){printText(text)}, 1000);
+        }
+
+        i++;
+    }
+
+    text += '> Complete! https://your-project.yourdomain.com';
+    setTimeout(function(){printText(text)}, 1000);
+}
+
+window.onload = function show_deploy_cli() {
+    setTimeout(function(){whilefun()}, 1000);
+};
+
+function printText(text) {
+    var p = document.querySelector('.new_cli_text');
+    p.innerHTML = text;
+}
