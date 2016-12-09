d9d8c46248915204a9f4bb939835f24b2ccb6843
lavr11 <lavrentev.srg@gmail.com>
2016-12-09 20:30:17 +0300

    delete branch

--- public/js/logs/error.js
+++
@@ -1,2 +0,0 @@
-/** @namespace print */
-print('<span class="w-cli__oe">invalid command.</span> Use <span class="w-cli__oi">lb help</span> to list valid commands');
--- public/js/logs/help.js
+++
@@ -1,2 +0,0 @@
-/** @namespace print */
-print('<span class="w-cli__oy">lb init &lt;app&gt;</span><span class="w-cli__om"> - Init Last.Backend project</span>', 0);
